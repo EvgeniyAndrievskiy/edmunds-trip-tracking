8bdf6e5aeb22389d40f2615e8715402313ec7fdb
unknown <Tsimur_Abayeu@EPBYMINL0002.minsk.epam.com>
2015-09-29 18:32:02 +0300

    Dealer inventory table

--- dealer-dashboard/src/main/webapp/WEB-INF/resources/scripts/scripts.js
+++ dealer-dashboard/src/main/webapp/WEB-INF/resources/scripts/scripts.js
@@ -1,16 +1,32 @@
 var tripMapContainerID = 'tripMapContainer';
 
-function init(){
-	initMap();
-}
+var dealerIDInputID = 'dealerIDInput';
+
+var dealerInventoryTableID = 'dealerInventoryTable';
+
+var DEFAULT_DEALER_ID = 90404;
 
 $(function() {
     init();
 });
+
+function init(){
+	$('#' + dealerIDInputID).val(DEFAULT_DEALER_ID); 
+    refreshDealerInventoryTable(DEFAULT_DEALER_ID);
+    $('#' + dealerInventoryTableID).bootstrapTable().on('post-body.bs.table', function (e, name, args) {
+    	refreshDealerInventoryTable($('#' + dealerIDInputID).val());
+    });
+}
 
 function initMap() {
 	  map = new google.maps.Map(document.getElementById(tripMapContainerID), {
 	    center: {lat: -34.397, lng: 150.644},
 	    zoom: 8
 	  });
-	}
+}
+
+function refreshDealerInventoryTable(dealerZipCode) {
+	$('#' + dealerInventoryTableID + ' td:nth-child(1)').wrap(function() {
+		return '<td><div class="imageContainer"><img src="' + $(this).text() + '"/></div></td>'
+		});
+}

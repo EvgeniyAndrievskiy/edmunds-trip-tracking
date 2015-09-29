8932b7cefd8862e81bf8ac8286ae011098e4d1f3
Yauheni Andryieuski <yandryieuski@edmunds.com>
2015-09-29 17:32:47 +0300

    Added SpeedLimitsService class.

---
+++ dealer-dashboard/src/main/java/com/edmunds/track/speed/SpeedLimitsService.java
@@ -0,0 +1,28 @@
+package com.edmunds.track.speed;
+
+import com.google.maps.GeoApiContext;
+import com.google.maps.RoadsApi;
+import com.google.maps.model.LatLng;
+import com.google.maps.model.SpeedLimit;
+
+import java.util.Collection;
+
+/**
+ * Created by Administrator on 9/29/2015.
+ */
+public class SpeedLimitsService {
+
+    private GeoApiContext geoApiContext;
+
+    public SpeedLimitsService() {
+        geoApiContext = new GeoApiContext();
+    }
+
+    public SpeedLimit[] getSpeedLimits(Collection<LatLng> pathPoints) throws Exception {
+        return RoadsApi.speedLimits(geoApiContext, pathPoints.toArray(new LatLng[pathPoints.size()])).await();
+    }
+
+    public void setApiKey(String apiKey) {
+        geoApiContext.setApiKey(apiKey);
+    }
+}

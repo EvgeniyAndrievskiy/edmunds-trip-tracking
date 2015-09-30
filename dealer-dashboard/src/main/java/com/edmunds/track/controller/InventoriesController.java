76fa450eb93e6953eba003b91fce09ef20727cc8
Yauheni Andryieuski <yandryieuski@edmunds.com>
2015-09-30 14:46:28 +0300

    Added ratings.

--- dealer-dashboard/src/main/java/com/edmunds/track/controller/InventoriesController.java
+++ dealer-dashboard/src/main/java/com/edmunds/track/controller/InventoriesController.java
@@ -3,8 +3,10 @@ package com.edmunds.track.controller;
 import com.edmunds.rest.dto.common.LinkDto;
 import com.edmunds.rest.dto.inventory.InventoriesDto;
 import com.edmunds.rest.dto.inventory.InventoryDto;
+import com.edmunds.track.criteria.RoadType;
 import com.edmunds.track.inventory.InventoryService;
 import com.edmunds.track.service.PathService;
+import com.edmunds.track.speed.SpeedLimitsAnalyzer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Administrator on 9/29/2015.
@@ -24,28 +25,23 @@ public class InventoriesController {
 
     @Autowired
     private InventoryService inventoryService;
-    @Autowired
-    private PathService pathService;
 
     @ResponseBody
     @RequestMapping(method= RequestMethod.GET, produces = "application/json")
-    public InventoriesResponse getInventories(String dealerId, long pathId) {
+    public InventoriesResponse getInventories(String dealerId, int pathId) {
         InventoriesDto inventoriesDto = inventoryService.getInventoriesByLocation(dealerId);
+        Map<String, Double> ratings = inventoryService.sortInventoriesByRelativity(pathId, inventoriesDto.getInventories());
         List<Inventory> inventories = new ArrayList<Inventory>();
-        for(int i = 0; i < inventoriesDto.getInventories().size(); i++) {
-            InventoryDto inventoryDto = inventoriesDto.getInventories().get(i);
-
+        for(InventoryDto inventoryDto : inventoriesDto.getInventories()) {
             String previewHref = "";
             if(inventoryDto.getMedia() != null) {
                 previewHref = inventoryDto.getMedia().getPhotos().getThumbnails().getLinks().iterator().next().getHref();
             }
-            Inventory inventory = new Inventory(previewHref, inventoryDto.getVin());
+            Inventory inventory = new Inventory(previewHref, inventoryDto.getVin(), ratings.get(inventoryDto.getId()));
             inventories.add(inventory);
         }
 
         return new InventoriesResponse(inventoriesDto.getInventories().get(0).getDealer().getName(), inventories);
     }
-
-
 
 }
--- dealer-dashboard/src/main/java/com/edmunds/track/inventory/InventoryService.java
+++ dealer-dashboard/src/main/java/com/edmunds/track/inventory/InventoryService.java
@@ -1,10 +1,17 @@
 package com.edmunds.track.inventory;
 
+import com.edmunds.rest.client.RequestView;
 import com.edmunds.rest.dto.inventory.InventoriesDto;
+import com.edmunds.rest.dto.inventory.InventoryDto;
+import com.edmunds.rest.dto.vehicle.StyleDto;
 import com.edmunds.rest.service.client.inventory.InventoryV2RestClient;
-import com.edmunds.rest.service.client.inventory.param.LocationParam;
+import com.edmunds.rest.service.client.vehicle.impl.StyleRestClientImpl;
+import com.edmunds.track.criteria.RoadType;
+import com.edmunds.track.service.PathService;
+import com.edmunds.track.speed.SpeedLimitsAnalyzer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 /**
  * Created by Administrator on 9/29/2015.
@@ -12,11 +19,66 @@ import org.springframework.stereotype.Service;
 //@Service
 public class InventoryService {
 
+    public static final int MAX_HORSE_POWER = 600;
+    public static final int MIN_HORSE_POWER = 60;
+
+    public static final int MAX_RATING = 5;
+
     @Autowired
     private InventoryV2RestClient inventoryRestClient;
+    @Autowired
+    private PathService pathService;
+    @Autowired
+    private SpeedLimitsAnalyzer speedLimitsAnalyzer;
+    @Autowired
+    private StyleRestClientImpl styleRestClient;
+
+    private Map<Long, Integer> engineHorsePowerMap = new HashMap<>();
 
     public InventoriesDto getInventoriesByLocation(String dealerId) {
         return inventoryRestClient.getInventoriesByDealerId(dealerId);
+    }
+
+    /**
+     * Sorts inventories by rating and returns these ratings.
+     *
+     * @param inventories inventories.
+     * @return ratings.
+     */
+    public Map<String, Double> sortInventoriesByRelativity(int pathId, List<InventoryDto> inventories) {
+        Map<RoadType, Double> pathMap = pathService.getRoadTypesWithPcts(pathId);
+        final double averageSpeedCoeff = speedLimitsAnalyzer.getSpeedLimitsAverageCoeff(pathMap.get(RoadType.HIGHWAY_RURAL),
+                pathMap.get(RoadType.HIGHWAY_URBAN), pathMap.get(RoadType.RESIDENTIAL));
+        fillEngineHorsePowerMap(inventories);
+        Map<String, Double> ratingMap = new HashMap<>();
+        for(InventoryDto inventoryDto : inventories) {
+            double delta = Math.abs(getInventoryHorsePowerCoeff(inventoryDto) - averageSpeedCoeff);
+            ratingMap.put(inventoryDto.getId(), (1 - delta) * MAX_RATING);
+        }
+        Collections.sort(inventories, new Comparator<InventoryDto>() {
+            @Override
+            public int compare(InventoryDto o1, InventoryDto o2) {
+                Double rating1 = ratingMap.get(o1.getId());
+                Double rating2 = ratingMap.get(o2.getId());
+                return rating2.compareTo(rating1);
+            }
+        });
+        return ratingMap;
+    }
+
+    private void fillEngineHorsePowerMap(List<InventoryDto> inventories) {
+        for(InventoryDto inventoryDto : inventories) {
+            if(!engineHorsePowerMap.containsKey(inventoryDto.getStyle().getId())) {
+                StyleDto styleDto = styleRestClient.findStyleById(inventoryDto.getStyle().getId(), RequestView.basic());
+                engineHorsePowerMap.put(inventoryDto.getStyle().getId(), styleDto.getEngine().getHorsepower());
+            }
+        }
+
+    }
+
+    private double getInventoryHorsePowerCoeff(InventoryDto inventoryDto) {
+        double horsePower = engineHorsePowerMap.get(inventoryDto.getStyle().getId());
+        return (horsePower - MIN_HORSE_POWER) / (MAX_HORSE_POWER - MIN_HORSE_POWER);
     }
 
 }
